d6c14e251087def14ecee37f8a1fdcf6ca6d867b
Alex <18011172+Pythalex@users.noreply.github.com>
2019-02-20 21:35:35 +0100

    Update removeRTQuotes.js

--- removeRTQuotes.js
+++ removeRTQuotes.js
@@ -1,27 +1,61 @@
 // ==UserScript==
-// @name         Remove RT Quotes
+// @name         remove RT quotes
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Remove RT quotes on Twitter
+// @description  remove RT quotes from twitter
 // @author       Hyorvenn
-// @match        https://twitter.com/
+// @match        https://twitter.com/*
 // @grant        none
 // ==/UserScript==
 
-// Hi
+var name = "[Remove RT quotes]";
+var tag = name + " : ";
+
+// ==========================
+
+//   CONFIGURE OPTION HERE
+//---------------------------
+
+var TL_only = true; // Remove RT quotes only of the timeline (main page)
+
+
+// ==========================
+
+function debug(message){
+    console.log(tag + message);
+}
+
+function on_tl(){
+    return window.location.href === "https://twitter.com/";
+}
 
 function start(){
     if (window.jQuery !== undefined){
         let quotes = jQuery("li[data-item-type='tweet']").has(".QuoteTweet");
         quotes.css("display", "none");
     } else {
-        console.log("[Remove RT Quotes] : Module loading");
+        debug("Module is loading ...");
     }
     setTimeout(start, 1000);
 }
 
 (function() {
+    // Don't run on frames or iframes
+    if (window.top != window.self){
+        return ;
+    }
     'use strict';
-    start();
+
+    let shouldstart = true;
+
+    if (TL_only){
+        shouldstart = shouldstart && on_tl();
+    }
+
+    if (shouldstart){
+        start();
+    } else {
+        debug(window.location.href);
+    }
 })();
 
